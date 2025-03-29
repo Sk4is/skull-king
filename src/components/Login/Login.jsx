7834093a00b7e20c26c7d7b0086d5093e5faf359
Usuario <adrianperezagredano@gmail.com>
2025-03-29 13:03:35 +0100

    frontend merged

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import './login.css';
+import './Login.css';
 import Swal from 'sweetalert2';
 
 const Login = () => {
